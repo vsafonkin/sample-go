36f63cbbc79a1a1a7a770ee5d681765a10308b91
vsafonkin <vsafonkin@gmail.com>
2023-02-01 12:42:53 +0300

    Test

--- sample.go
+++ sample.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 
+	fmt.Println("--- hello ---")
 	bnum := 0b11111110
 	fmt.Println(bnum)
 
@@ -14,11 +15,16 @@ func main() {
 
 	unicode := '\u0061'
 	fmt.Println(unicode)
-	fmt.Println("-----")
+	fmt.Println("--- types ---")
 
 	var test float64 = 254.1
 	result := int(test)
 	fmt.Println(result)
 	fmt.Printf("Type: %T\nBinary: %b\nHex: %X\n", result, result, result)
 	fmt.Printf("%d\n", 0b11111011)
+
+	fmt.Println("--- arrays ---")
+
+	var arr [3]string
+	fmt.Println(arr)
 }
