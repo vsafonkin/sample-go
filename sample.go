60c3caf09822b4fd86d7d66112f0e0453011d061
vsafonkin <vsafonkin@gmail.com>
2023-03-18 19:24:49 +0300

    Test

--- sample.go
+++ sample.go
@@ -8,20 +8,4 @@ import (
 func main() {
 	var currentTime = time.Now()
 	fmt.Println(currentTime)
-
-	bnum := 0b11111110
-	fmt.Println(bnum)
-
-	xnum := 0xFE
-	fmt.Println(xnum)
-
-	unicode := '\u0061'
-	fmt.Println(unicode)
-
-	var f float64 = 254.1
-	result := int(f)
-	fmt.Println(result)
-	fmt.Printf("Type: %T\nBinary: %b\nHex: %X\n", result, result, result)
-	fmt.Printf("%d\n", 0b11111011)
-
 }
