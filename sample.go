a8de2299d19a98550bc640b23bc308eee0041d4a
vsafonkin <vsafonkin@gmail.com>
2023-03-18 19:29:18 +0300

    Test

--- sample.go
+++ sample.go
@@ -5,7 +5,28 @@ import (
 	"time"
 )
 
+type User struct {
+	name string
+	age  int
+}
+
 func main() {
 	var currentTime = time.Now()
 	fmt.Println(currentTime)
+	fmt.Printf("-----\n\n")
+
+	bob := User{
+		name: "bob",
+		age:  123,
+	}
+
+	fmt.Println(bob)
+	sayHello(bob)
+	fmt.Println(bob)
+}
+
+func sayHello(user User) error {
+	fmt.Printf("Hello %s\n", user.name)
+	user.name = "alisa"
+	return nil
 }
