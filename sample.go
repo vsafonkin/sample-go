0eefa4e20972c02dfd89ba0319ac044a5068ad02
vsafonkin <vsafonkin@gmail.com>
2023-03-23 18:58:28 +0300

    Test

--- sample.go
+++ sample.go
@@ -18,43 +18,4 @@ func main() {
 	var currentTime = time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
-
-	bob := User{
-		name: "Bob",
-		age:  123,
-	}
-	fmt.Println(bob.toString())
-	bob.setName("Alisa")
-	fmt.Println(bob.toString())
-
-	runTest(bob)
-
-	data := map[string]interface{}{
-		"a": "hello",
-		"b": 123,
-	}
-	data["c"] = true
-	fmt.Println(data)
-
-	sample := make(map[string]int, 10)
-	fmt.Println(len(sample), sample)
-	sample["test"] = 123
-	fmt.Println(len(sample), sample)
-}
-
-func (u User) toString() string {
-	return fmt.Sprintf("User name: %s, User age: %d\n", u.name, u.age)
-}
-
-func (u *User) setName(name string) {
-	u.name = name
-}
-
-func (u User) Test() {
-	fmt.Println("User Test")
-}
-
-func runTest(test Tester) error {
-	test.Test()
-	return nil
 }
