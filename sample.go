93ec75ecf05acad6270f461a6494cc07188cb201
vsafonkin <vsafonkin@gmail.com>
2023-03-25 22:17:57 +0300

    Test

--- sample.go
+++ sample.go
@@ -10,10 +10,6 @@ type User struct {
 	age  int
 }
 
-type Admin struct {
-	name string
-}
-
 type Tester interface {
 	Test()
 }
@@ -23,32 +19,4 @@ func main() {
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
 
-	bob := User{
-		name: "Bob",
-		age:  123,
-	}
-
-	var tester Tester = bob
-	runTest(tester)
-
-	sayHello(tester.(User))
-	runTest(tester.(User))
-
-}
-
-func runTest(tester Tester) error {
-	tester.Test()
-	return nil
-}
-
-func (u User) Test() {
-	fmt.Println("User test method")
-}
-
-func (u Admin) Test() {
-	fmt.Println("Admin test method")
-}
-
-func sayHello(user User) {
-	fmt.Printf("Hello, %s\n", user.name)
 }
