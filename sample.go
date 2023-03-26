8e886bdfa322df3d675615b57d38e9a47bfae096
vsafonkin <vsafonkin@gmail.com>
2023-03-26 23:14:01 +0300

    Test

--- sample.go
+++ sample.go
@@ -2,39 +2,12 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"time"
 )
-
-type User struct {
-	name string
-	age  int
-}
-
-type Tester interface {
-	Test()
-}
 
 func main() {
 	var currentTime = time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
 
-	matched, err := regexp.MatchString("a+b=c", "a+b=c")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(matched)
-
-	reg, err := regexp.Compile("h.?w")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	result := reg.FindAllString("hello hew heew ohowa", -1)
-	fmt.Println(result)
-
-	reg, _ = regexp.Compile(`\d+?`)
-	result = reg.FindAllString("a123b456c789", -1)
-	fmt.Println(result)
 }
