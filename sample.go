c0fa4d501e085566e9fce380787f0ba584e3661e
vsafonkin <vsafonkin@gmail.com>
2023-03-28 10:57:49 +0300

    Test

--- sample.go
+++ sample.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/vsafonkin/sample-go/user"
 )
 
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
+
+	bob := user.User{
+		Name: "Bob",
+	}
+
+	bob.SayHello()
 }
