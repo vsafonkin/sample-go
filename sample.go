50c1bce2c38d6c87c03865c3eb7ffa0143cc932d
vsafonkin <vsafonkin@gmail.com>
2023-03-29 19:25:33 +0300

    Goroutines

--- sample.go
+++ sample.go
@@ -15,13 +15,19 @@ func main() {
 	run(456, ch)
 	run(789, ch)
 
-	fmt.Println(<-ch)
-	time.Sleep(2 * time.Second)
+	for v := range ch {
+		fmt.Println(v)
+	}
 }
 
 func run(id int, ch chan<- int) {
+	i := id
 	go func() {
 		ch <- id
+		if i == 789 {
+			time.Sleep(2 * time.Second)
+			close(ch)
+		}
 		fmt.Printf("End goroutine id: %d\n", id)
 	}()
 }
