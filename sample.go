792a7325fd2820fba43770c8425802319a4b7ae6
vsafonkin <vsafonkin@gmail.com>
2023-03-30 13:16:34 +0300

    Test

--- sample.go
+++ sample.go
@@ -19,8 +19,9 @@ func main() {
 		id := i
 		go func() {
 			defer wg.Done()
-			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-			fmt.Printf("Close grt %d\n", id)
+			rnum := rand.Intn(1000)
+			time.Sleep(time.Duration(rnum) * time.Millisecond)
+			fmt.Printf("Close grt %d, interval %d ms\n", id, rnum)
 		}()
 	}
 	wg.Wait()
