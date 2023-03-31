d8fcb53a4aeffecb805c830e0f5444a54eb2cbe0
vsafonkin <vsafonkin@gmail.com>
2023-03-31 11:49:40 +0300

    Test

--- sample.go
+++ sample.go
@@ -1,67 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"time"
 )
-
-type User struct {
-	Id       int     `json:"id"`
-	Name     string  `json:"name"`
-	Username string  `json:"username"`
-	Email    string  `json:"email"`
-	Address  Address `json:"address"`
-}
-
-type Address struct {
-	City    string `json:"city"`
-	Street  string `json:"street"`
-	ZipCode string `json:"zipcode"`
-}
-
-type Users []User
 
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
 
-	users := Users{}
-	users.loadUserData("users.json")
-	fmt.Printf("%+v\n", users)
-
-	users.writeToJsonFile("sample.json")
-}
-
-func (users *Users) loadUserData(path string) error {
-	dataFile, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(dataFile, users)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (users *Users) writeToJsonFile(path string) error {
-	outputFile, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer outputFile.Close()
-	data, err := json.Marshal(users)
-	if err != nil {
-		return err
-	}
-	_, err = outputFile.Write(data)
-	if err != nil {
-		return err
-	}
-	return nil
 }
