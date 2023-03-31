cb93833e962d94d222cc992a698e0213c9c3252c
vsafonkin <vsafonkin@gmail.com>
2023-03-31 11:11:41 +0300

    Test

--- sample.go
+++ sample.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"time"
 )
 
@@ -12,13 +14,27 @@ type User struct {
 	Email    string `json:"email"`
 }
 
-type Users struct {
-	Users []User
-}
+type Users []User
 
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
 
+	users := Users{}
+	users.loadUserData("users.json")
+	fmt.Println(users)
+}
+
+func (users *Users) loadUserData(path string) error {
+	dataFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(dataFile, users)
+	if err != nil {
+		return err
+	}
+	return nil
 }
