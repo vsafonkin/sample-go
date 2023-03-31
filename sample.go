abe4a23c044fc78b6d85cae96491ad92bec95928
vsafonkin <vsafonkin@gmail.com>
2023-03-31 11:06:08 +0300

    Test

--- sample.go
+++ sample.go
@@ -2,40 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
 	"time"
 )
+
+type User struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type Users struct {
+	Users []User
+}
 
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
 
-	var wg sync.WaitGroup
-	num := 10
-	wg.Add(num)
-
-	out := make(chan int)
-	for i := 0; i < num; i++ {
-		id := i
-		go func() {
-			defer wg.Done()
-			rnum := rand.Intn(3000)
-			time.Sleep(time.Duration(rnum) * time.Millisecond)
-			fmt.Printf("Close grt %d, interval %d ms\n", id, rnum)
-			out <- rnum
-		}()
-	}
-	go func() {
-		wg.Wait()
-		close(out)
-	}()
-
-	var result []int
-	for v := range out {
-		result = append(result, v)
-	}
-
-	fmt.Printf("Result: %v\n", result)
 }
