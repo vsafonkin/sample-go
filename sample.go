dbe84c1d975057077f8a7b2048f52d7c9595afd6
vsafonkin <vsafonkin@gmail.com>
2023-04-03 13:48:10 +0300

    Revert

--- sample.go
+++ sample.go
@@ -3,20 +3,10 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/vsafonkin/sample-go/stdlib_json"
 )
 
 func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
-
-	usersData := stdlib_json.Users{}
-	err := usersData.LoadUserData("./test_data/users.json")
-	if err != nil {
-		fmt.Println("error: ", err)
-	}
-	fmt.Printf("Users data: %+v\n", usersData)
-	usersData.WriteDataToFile("./test_data/new_users.json")
 }
