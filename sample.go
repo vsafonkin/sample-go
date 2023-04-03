0e9635bd2653fd384b1da7f784ae27ff8c46f4c2
vsafonkin <vsafonkin@gmail.com>
2023-04-03 14:02:22 +0300

    Test

--- sample.go
+++ sample.go
@@ -9,4 +9,10 @@ func main() {
 	currentTime := time.Now()
 	fmt.Println(currentTime)
 	fmt.Printf("-----\n\n")
+
+	b := 0b11100100
+	x := 0xE4
+	num := 228
+	fmt.Println(b, x)
+	fmt.Printf("decimal: %d, bin: %#[1]b, hex:%#[1]x\n", num)
 }
