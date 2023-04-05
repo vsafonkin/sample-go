c7b42358a6e2ff06b27cca403860649f7765a385
vsafonkin <vsafonkin@gmail.com>
2023-04-05 11:53:58 +0300

    Test

--- sample.go
+++ sample.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Day int
+
+const (
+	_ Day = iota
+	Sunday
+	Monday
+)
+
 func main() {
 	fmt.Println("-----")
+	showDayValue(Sunday)
+}
+
+func showDayValue(d Day) {
+	fmt.Printf("Day: %d\n", d)
 }
