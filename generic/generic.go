1f687c0bc910fb16768abcdd4d8c7cffdabfce2f
vsafonkin <vsafonkin@gmail.com>
2023-04-05 18:47:43 +0300

    Test

--- generic/generic.go
+++ generic/generic.go
@@ -14,6 +14,11 @@ func HandleMap[K comparable, V any](m map[K]V) ([]K, []V) {
 	return keys, values
 }
 
+func HandleSlice[T any, V any](arg T, slc []V) {
+	fmt.Println("First arg:", arg)
+	fmt.Println("Slice:", slc)
+}
+
 func Run() error {
 	m := map[string]int{}
 	m["hello"] = 123
@@ -21,5 +26,11 @@ func Run() error {
 	keys, values := HandleMap(m)
 	fmt.Println("Keys slice:", keys)
 	fmt.Println("Values slice:", values)
+	fmt.Println("-----")
+
+	slc := []bool{true, false, true}
+	slc_2 := []int{7, 11, 13}
+	HandleSlice("Hello", slc)
+	HandleSlice(1.2e3, slc_2)
 	return nil
 }
