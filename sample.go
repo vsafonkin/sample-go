ce30e0bb66c95172e54d44dc4ab249a99b7e3d3c
vsafonkin <vsafonkin@gmail.com>
2023-04-05 11:57:58 +0300

    Test

--- sample.go
+++ sample.go
@@ -10,11 +10,20 @@ const (
 	Monday
 )
 
+type Username string
+
 func main() {
 	fmt.Println("-----")
 	showDayValue(Sunday)
+	sayHello("Bob")
+	var alisa Username = "Alisa"
+	fmt.Println(alisa + "bob")
 }
 
 func showDayValue(d Day) {
 	fmt.Printf("Day: %d\n", d)
+}
+
+func sayHello(name Username) {
+	fmt.Printf("Hey, %s\n", name)
 }
