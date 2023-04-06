a21e6579a6b0233f389e3b26c6cd5991b70fc6a7
vsafonkin <vsafonkin@gmail.com>
2023-04-06 15:02:36 +0300

    Test

--- sample.go
+++ sample.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+type Fn func(str string) int
+
 func main() {
 	fmt.Println("-----")
+
+	var arr []int
+	fmt.Println(arr == nil)
+
+	doSomething("hello", func(str string) int { return len(str) })
+}
+
+func doSomething(str string, fn Fn) {
+	fmt.Println(fn(str))
 }
