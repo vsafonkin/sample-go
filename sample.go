1a3f877a71f332375cbc16a33b3c2898a04a9064
vsafonkin <vsafonkin@gmail.com>
2023-04-06 19:49:46 +0300

    Test

--- sample.go
+++ sample.go
@@ -4,24 +4,9 @@ import (
 	"fmt"
 )
 
-type Fn func()
-
 func main() {
 	fmt.Println("-----")
 
-	DoSomething, counter := func() (Fn, *int) {
-		var counter int
-		return func() {
-			counter++
-			DoSomething()
-		}, &counter
-	}()
-	fmt.Println(*counter)
-	DoSomething()
-	fmt.Println(*counter)
-	DoSomething()
-	DoSomething()
-	fmt.Println(*counter)
 }
 
 func DoSomething() {
