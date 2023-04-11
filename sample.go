cc3dcedc31a68b29673170c6d80707bcd4ef1ff4
vsafonkin <vsafonkin@gmail.com>
2023-04-11 16:24:49 +0300

    Test

--- sample.go
+++ sample.go
@@ -6,4 +6,15 @@ import (
 
 func main() {
 	fmt.Println("-----")
+	slc := []int{1, 2, 3}
+	setFirst(slc)
+	fmt.Println(slc)
+}
+
+func setFirst(slc []int) error {
+	if len(slc) == 0 {
+		return fmt.Errorf("slice len is zero")
+	}
+	slc[0] = 123
+	return nil
 }
