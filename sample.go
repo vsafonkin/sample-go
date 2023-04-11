62604f9e8e38b774ef42a85f80ee13df007cf273
vsafonkin <vsafonkin@gmail.com>
2023-04-11 16:04:39 +0300

    Test

--- sample.go
+++ sample.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	fmt.Println("-----")
 
-	f := 12.345
-	fmt.Printf("%f %[1]e\n", f)
-
-	var p *int
-	fmt.Println(p)
-
-	t := reflect.TypeOf(p)
-
-	fmt.Println(t.Name())
 }
 
 func DoSomething() {
