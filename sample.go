81c4ab061254c14059e1e79272b7df1c24cd93df
vsafonkin <vsafonkin@gmail.com>
2023-04-13 17:58:50 +0300

    Test

--- sample.go
+++ sample.go
@@ -4,42 +4,6 @@ import (
 	"fmt"
 )
 
-type mSlc []int
-
 func main() {
 	fmt.Println("-----")
-	slc := []int{1, 2, 3}
-	setFirst(slc)
-	fmt.Println(slc)
-
-	m := map[string]int{
-		"hello":   1,
-		"goodbye": 2,
-	}
-
-	mHelloKey(m)
-	fmt.Printf("%+v\n", m)
-
-	var mslc mSlc
-	fmt.Println(mslc.getLen())
-}
-
-func setFirst(slc []int) error {
-	if len(slc) == 0 {
-		return fmt.Errorf("slice len is zero")
-	}
-	slc[0] = 123
-	return nil
-}
-
-func mHelloKey(m map[string]int) error {
-	if len(m) == 0 {
-		return fmt.Errorf("empty map")
-	}
-	m["hello"] = 789
-	return nil
-}
-
-func (mslc mSlc) getLen() int {
-	return len(mslc)
 }
