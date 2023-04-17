f028ef2b17b6353db8e97e54b367e46d8862ece2
vsafonkin <vsafonkin@gmail.com>
2023-04-17 19:20:58 +0300

    Test

--- sample.go
+++ sample.go
@@ -6,13 +6,4 @@ import (
 
 func main() {
 	fmt.Println("-----")
-
-	var slc []int
-	addItem(5, slc)
-	fmt.Println(slc)
-}
-
-func addItem(item int, slc []int) {
-	slc = append(slc, item)
-	fmt.Println("addItem slc:", slc)
 }
