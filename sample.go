a61de8a3ca813ee6e179d184e4a5473fc4126b01
vsafonkin <vsafonkin@gmail.com>
2023-04-18 15:04:15 +0300

    Test

--- sample.go
+++ sample.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("-----")
+
+	slc := []int{1, 3, 5, 7}
+	fmt.Println(sumIntSlice(slc...))
+	fmt.Println(sumIntSlice(2, 4, 6))
+}
+
+func sumIntSlice(nums ...int) int {
+	var sum int
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
 }
