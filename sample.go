01604d926f973dd083a64af8199a8498b7c3dcff
vsafonkin <vsafonkin@gmail.com>
2023-04-18 20:01:01 +0300

    Test

--- sample.go
+++ sample.go
@@ -5,12 +5,24 @@ import (
 )
 
 type Runner interface {
-	Run()
+	Run() error
 }
+
+type Test int
 
 func main() {
 	fmt.Println("-----")
 
-	var runner Runner
-	fmt.Println(runner)
+	var runner Test
+	err := Exec(runner)
+	fmt.Println(err)
+}
+
+func Exec(runner Runner) error {
+	return runner.Run()
+}
+
+func (t Test) Run() error {
+	fmt.Println("Test m")
+	return nil
 }
