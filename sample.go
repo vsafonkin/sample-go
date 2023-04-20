3fdf74fdf701ade5a5a7b764e2621bac8c6234b3
vsafonkin <vsafonkin@gmail.com>
2023-04-20 18:28:22 +0300

    Test

--- sample.go
+++ sample.go
@@ -4,34 +4,6 @@ import (
 	"fmt"
 )
 
-type Runner interface {
-	Run() error
-}
-
-type Tester interface {
-	StartTest() error
-}
-
-type Process struct {
-	id int
-	Runner
-	Tester
-}
-
 func main() {
 	fmt.Println("-----")
-
-	var p Process
-	fmt.Printf("id: %d\n", p.id)
-	err := Exec(p)
-	fmt.Println(err)
-}
-
-func Exec(runner Runner) error {
-	return runner.Run()
-}
-
-func (a Process) Run() error {
-	fmt.Println("Test m")
-	return nil
 }
