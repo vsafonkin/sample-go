d18a7cf3935079465c192b87a509d1383dd94fe8
vsafonkin <vsafonkin@gmail.com>
2023-05-08 15:27:15 +0300

    Test

--- sample.go
+++ sample.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
-
-type Runner struct {
-	id int
-}
 
 func main() {
 	fmt.Println("-----")
 
-	var runner *Runner
-	fmt.Println(runner, runner == nil)
-	runner.run()
+	n := 1_000_000
+	fmt.Printf("Total time string concat: %v\n", funcExecTimeDuration(runStringConcat, n))
+	fmt.Printf("Total time string builder: %v\n", funcExecTimeDuration(runStringBuilder, n))
 }
 
-func (r *Runner) run() error {
-	fmt.Println("Run...")
-	return nil
+func runStringConcat(n int) {
+	var result string
+	for i := 0; i < n; i++ {
+		result += strconv.Itoa(i)
+	}
+}
+
+func runStringBuilder(n int) {
+	var result strings.Builder
+	for i := 0; i < n; i++ {
+		result.WriteString(strconv.Itoa(i))
+	}
+}
+
+func funcExecTimeDuration(fn func(int), n int) time.Duration {
+	start := time.Now()
+	fn(n)
+	return time.Since(start)
 }
