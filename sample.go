e43776c822120665ebf5eb4e46a77e4175c0f505
vsafonkin <vsafonkin@gmail.com>
2023-05-29 20:06:13 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,31 +2,8 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func main() {
 	fmt.Println("-----")
-}
-
-func runStringConcat(n int) {
-	var result string
-	for i := 0; i < n; i++ {
-		result += strconv.Itoa(i)
-	}
-}
-
-func runStringBuilder(n int) {
-	var result strings.Builder
-	for i := 0; i < n; i++ {
-		result.WriteString(strconv.Itoa(i))
-	}
-}
-
-func funcExecTimeDuration(fn func(int), n int) time.Duration {
-	start := time.Now()
-	fn(n)
-	return time.Since(start)
 }
