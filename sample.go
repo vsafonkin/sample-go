9e6b450e461908e9586cf492b2924698b8fa59fe
vsafonkin <vsafonkin@gmail.com>
2023-06-23 15:54:45 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,27 +6,4 @@ import (
 
 func main() {
 	fmt.Println("hello")
-
-	n := 123_456
-	x := 0xEA
-	b := 0b11101010
-	f := 1.23e2
-	r := '\uEA12'
-
-	fmt.Println(n, x, b, f, r)
-
-	s := 123 + 1.23 + '\uEA12'
-	fmt.Println(s)
-
-	str := `
-	Hello
-	"Goodbye"
-	\n\t\r
-	`
-
-	fmt.Println(str)
-
-	for i := range [5]int{} {
-		fmt.Println(i)
-	}
 }
