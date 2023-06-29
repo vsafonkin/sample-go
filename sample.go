5decf36102d011505a5f6d38c5abb340db253af6
vsafonkin <vsafonkin@gmail.com>
2023-06-29 20:19:39 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
 	fmt.Println("hello")
 
-	arr := make([]int, 512)
-	fmt.Println(len(arr), cap(arr))
-	arr = append(arr, 1)
-	fmt.Println(len(arr), cap(arr))
+	var arr = [100]struct{}{}
+	var p unsafe.Pointer
+	var st = struct {
+		name string
+		age  byte
+	}{
+		name: "Bob",
+		age:  123,
+	}
+	for i := range arr {
+		fmt.Println(i)
+	}
+	fmt.Println("size:", unsafe.Sizeof(arr))
+	fmt.Println(st, unsafe.Sizeof(st))
+	fmt.Println(p, unsafe.Sizeof(p))
 }
