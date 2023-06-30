51ef13e1729a4bf33bcdb1c7beb0108a729cb70a
vsafonkin <vsafonkin@gmail.com>
2023-06-30 17:13:28 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
 	fmt.Println("hello")
-
-	var arr = [100]struct{}{}
-	var p unsafe.Pointer
-	var st = struct {
-		name string
-		age  byte
-	}{
-		name: "Bob",
-		age:  123,
-	}
-	for i := range arr {
-		fmt.Println(i)
-	}
-	fmt.Println("size:", unsafe.Sizeof(arr))
-	fmt.Println(st, unsafe.Sizeof(st))
-	fmt.Println(p, unsafe.Sizeof(p))
 }
