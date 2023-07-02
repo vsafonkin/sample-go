4b819f9ae8ff911792c242728033abba59a7ec07
vsafonkin <vsafonkin@gmail.com>
2023-07-02 22:57:58 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,13 +6,4 @@ import (
 
 func main() {
 	fmt.Println("hello")
-
-	arr := []int{1, 2, 3, 4, 5}
-	sl := arr[:]
-	sl = append(sl, 6)
-	fmt.Printf("arr: %v, sl: %v\n", arr, sl)
-	sl[0] = 7
-	fmt.Printf("arr: %v, sl: %v\n", arr, sl)
-	arr[1] = 8
-	fmt.Printf("arr: %v, sl: %v\n", arr, sl)
 }
