410860c961f53cb4a27510266dadd5b1a03dc1cf
vsafonkin <vsafonkin@gmail.com>
2023-07-03 18:30:25 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("hello")
+
+	m := make(map[string]int, 0)
+	m["hello"] = 123
+	fmt.Println(m)
+	m["hello"]++
+	fmt.Println(m)
+
+	if v, ok := m["hello"]; ok {
+		fmt.Println(ok, v)
+	}
+
+	if v, ok := m["goodbye"]; ok {
+		fmt.Println(ok, v)
+	}
 }
