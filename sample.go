f6d6808bf2014c7989efca477bd219583ceff5d2
vsafonkin <vsafonkin@gmail.com>
2023-07-07 18:29:58 +0300

    Debug

--- sample.go
+++ sample.go
@@ -4,32 +4,6 @@ import (
 	"fmt"
 )
 
-type User struct {
-	name string
-	age  int
-}
-
 func main() {
 	fmt.Println("hello")
-
-	bob := User{
-		name: "Bob",
-		age:  123,
-	}
-
-	var alisa User
-
-	fmt.Println(bob)
-	fmt.Println(alisa)
-
-	mike := User{"Bob", 123}
-	fmt.Println(mike == bob)
-
-	anonim := struct {
-		name string
-	}{
-		name: "John",
-	}
-
-	fmt.Println(anonim)
 }
