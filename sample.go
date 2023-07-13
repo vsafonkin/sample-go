6a13c3de0e8291130900ec107089f03793969981
vsafonkin <vsafonkin@gmail.com>
2023-07-13 11:32:53 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println("hello")
 
-	n := -1
-	if n := rand.Intn(100); n > 0 {
-		fmt.Println(n)
+	arr := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		go addIntToArray(arr, i)
 	}
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("%v\n", arr)
+	fmt.Printf("%p\n", &arr)
+	arr = append(arr, 123)
+	fmt.Printf("%v\n", arr)
+	fmt.Printf("%p\n", &arr)
 
-	fmt.Println(n)
+}
+
+func addIntToArray(arr []int, n int) {
+	arr[n] = n
+	arr = append(arr, 789)
 }
