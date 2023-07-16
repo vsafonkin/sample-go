7a885214bb3ceec7833271ecbf099d730f32ff1c
vsafonkin <vsafonkin@gmail.com>
2023-07-16 21:36:43 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,18 +6,4 @@ import (
 
 func main() {
 	fmt.Println("hello")
-
-	names := []string{"bob", "alisa", "mike", "test"}
-	for i, v := range names {
-		switch h := "hello"; v {
-		case "bob":
-			fmt.Println(i, h, "Bob case")
-		case "alisa":
-			fmt.Println(i, "Alisa case")
-		case "mike":
-			fmt.Println(i, "Mike case")
-		default:
-			fmt.Println(i, "Default case")
-		}
-	}
 }
