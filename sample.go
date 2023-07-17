53473e6629b1d6c7c318c046d88892f39ebb6023
vsafonkin <vsafonkin@gmail.com>
2023-07-17 18:59:21 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,14 +6,4 @@ import (
 
 func main() {
 	fmt.Println("hello")
-
-	names := []string{"bob", "alisa", "mike"}
-	test(names...)
-	test("hello", "goodbye")
-}
-
-func test(names ...string) {
-	for i, v := range names {
-		fmt.Printf("%d: %s\n", i, v)
-	}
 }
