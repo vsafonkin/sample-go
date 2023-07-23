1a840d16030603750b2451168c75b5feaa58b6d9
vsafonkin <vsafonkin@gmail.com>
2023-07-24 00:49:49 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 )
 
 func main() {
-	defer test("main")
 	fmt.Println("hello")
-	go func() {
-		defer test("anonim grt")
-		log.Printf("%s\n", "goroutine")
-	}()
-	time.Sleep(100 * time.Millisecond)
-}
-
-func test(str string) {
-	log.Printf("%s: %s\n", "defer", str)
 }
