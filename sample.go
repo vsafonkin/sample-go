24dae4a734b7c11167ed3afb778e8741087f526a
vsafonkin <vsafonkin@gmail.com>
2023-07-26 12:44:36 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,16 +6,4 @@ import (
 
 func main() {
 	fmt.Println("hello, pointers")
-
-	var p *int
-	fmt.Println(p == nil)
-	fmt.Printf("%p\n", p)
-	updatePointer(p)
-	fmt.Printf("%p\n", p)
-}
-
-func updatePointer(p *int) {
-	fmt.Printf("%p, %t\n", p, p == nil)
-	temp := 123
-	p = &temp
 }
