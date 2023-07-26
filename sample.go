1224628b82a951fd7e6171d571455258bda802da
vsafonkin <vsafonkin@gmail.com>
2023-07-26 13:00:18 +0300

    Debug

--- sample.go
+++ sample.go
@@ -6,4 +6,20 @@ import (
 
 func main() {
 	fmt.Println("hello, pointers")
+
+	arr := [3]int{1, 2, 3}
+	arrCopy := arr
+
+	fmt.Printf("arr: %v\narrCopy: %v\n", arr, arrCopy)
+	arr[0] = 789
+	fmt.Printf("arr: %v\narrCopy: %v\n", arr, arrCopy)
+
+	slc := []int{1, 2, 3}
+	slcCopy := slc
+
+	fmt.Printf("slc: %v\nslcCopy: %v\n", slc, slcCopy)
+	slc[0] = 789
+	fmt.Printf("slc: %v\nslcCopy: %v\n", slc, slcCopy)
+	slcCopy[0] = 456
+	fmt.Printf("slc: %v\nslcCopy: %v\n", slc, slcCopy)
 }
