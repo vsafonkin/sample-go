f19d3862fefe6e31c0d2561f187c77e17a69d5ac
vsafonkin <vsafonkin@gmail.com>
2023-08-03 10:04:30 +0300

    Debug

--- sample.go
+++ sample.go
@@ -8,16 +8,4 @@ type TestError struct{}
 
 func main() {
 	fmt.Println("hello")
-
-	err := test()
-	fmt.Println(err == nil)
-}
-
-func test() error {
-	var testError TestError
-	return testError
-}
-
-func (te TestError) Error() string {
-	return "test error m"
 }
