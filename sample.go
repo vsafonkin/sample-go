ca2d45bbdecea2439cc34af0cc02c5f1d9c0f66c
vsafonkin <vsafonkin@gmail.com>
2023-08-04 11:04:18 +0300

    Debug

--- sample.go
+++ sample.go
@@ -4,17 +4,6 @@ import (
 	"fmt"
 )
 
-type Tester interface{}
-
 func main() {
 	fmt.Println("hello")
-
-	var t Tester = 123
-
-	switch t := t.(type) {
-	case string:
-		fmt.Println("string type, conc2:", t+t)
-	case int:
-		fmt.Println("int type, mul3:", t*3)
-	}
 }
