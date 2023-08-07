2e54f1b1de8204faaf8321b125f584af9844a459
vsafonkin <vsafonkin@gmail.com>
2023-08-07 14:52:26 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type TestError struct {
-	Message string
+	Test      string
+	Message   string
+	ErrStatus int
 }
 
 func (e TestError) Error() string {
-	return e.Message
+	return fmt.Sprintf("error message: %s, error status: %d\n", e.Message, e.ErrStatus)
 }
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("-------------")
+	if _, err := openFile("./makefilee"); err != nil {
+		fmt.Println(err)
+	}
+
+	recPanic()
+	fmt.Println("goodbye")
+}
+
+func openFile(path string) (*os.File, error) {
+	return os.Open(path)
+}
+
+func throwPanic() {
+	panic(TestError{
+		Test:      "test message",
+		Message:   "stop program",
+		ErrStatus: 123,
+	})
+}
+
+func recPanic() {
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Printf("recover panic: %v\n", v)
+		}
+	}()
+	throwPanic()
 }
