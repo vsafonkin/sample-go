a88c3c2669db47b1a0fc2585db985f614ab11eca
vsafonkin <vsafonkin@gmail.com>
2023-08-29 16:53:58 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,45 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"sync"
 	"time"
 )
 
-type TestError struct {
-	Message   string
-	ErrStatus int
-}
-
-func (e TestError) Error() string {
-	return fmt.Sprintf("error message: %s, error status: %d\n", e.Message, e.ErrStatus)
-}
-
 func main() {
-	fmt.Println("-------------")
-
-	var wg sync.WaitGroup
-	wg.Add(3)
-
-	done := make(chan struct{})
-
-	go run(50*time.Millisecond, &wg)
-	go run(100*time.Millisecond, &wg)
-	go run(1000*time.Millisecond, &wg)
-	go func() {
-		defer close(done)
-		fmt.Println("wait all go")
-		wg.Wait()
-		fmt.Println("all gors are done")
-	}()
-	<-done
-
+	fmt.Println("hello")
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	ch := make(chan int)
+	go run(ctx, ch)
+	go doSomething(ch)
+	time.Sleep(100 * time.Millisecond)
+	cancel()
+	time.Sleep(100 * time.Millisecond)
 }
 
-func run(timeout time.Duration, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("go start")
-	fmt.Println("wait...", timeout, "milliseconds")
-	time.Sleep(timeout)
-	fmt.Println(timeout, "done")
+func run(ctx context.Context, ch <-chan int) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("run func is done")
+	case <-ch:
+		fmt.Println("read from ch")
+	}
+}
+
+func doSomething(ch chan<- int) {
+	defer close(ch)
+	time.Sleep(150 * time.Millisecond)
+	ch <- 123
 }
