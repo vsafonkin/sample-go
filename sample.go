dbbb1a08090c0a123c49f2c61a15c2fd34161b48
vsafonkin <vsafonkin@gmail.com>
2023-09-01 17:16:42 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("hello")
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 175*time.Millisecond)
+	go func() {
+		defer cancel()
+		now := time.Now()
+		<-ctx.Done()
+		fmt.Println("Timeout:", time.Since(now))
+	}()
+
+	time.Sleep(500 * time.Millisecond)
 }
