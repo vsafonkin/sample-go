ee527b57626a61654b4f761c3f9fcaa253f67d83
vsafonkin <vsafonkin@gmail.com>
2023-09-05 10:03:24 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	fmt.Println("hello")
-	ctx := context.Background()
-	t := reflect.TypeOf(ctx)
-	fmt.Println(t.Kind())
+	doSomething()
+}
 
-	m := map[string]int{
-		"hello": 123,
-	}
-	fmt.Println(reflect.TypeOf(m).Kind())
-
-	ptr := new(string)
-	fmt.Println(ptr)
-
-	*ptr = "hello"
-	fmt.Println(*ptr)
-
-	fmt.Println(reflect.TypeOf(ptr).Kind())
+func doSomething() {
+	fmt.Println("do something")
 }
