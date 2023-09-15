cfd8dce14d5aadc0a7a764aeb2dadc9e1796c6b1
vsafonkin <vsafonkin@gmail.com>
2023-09-15 15:30:55 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("hello")
+	dat, err := os.Open("./test_data/users.json")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(dat)
 }
