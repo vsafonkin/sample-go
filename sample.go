5f5bc0b5bb31abeccc938a80d44ef2b7b83f8047
vsafonkin <vsafonkin@gmail.com>
2023-09-15 19:33:36 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fmt.Println("hello")
-	dat, err := os.Open("./test_data/users.json")
+	f, err := os.Open("./test_data/users.json")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println(dat)
+
+	buf := make([]byte, 128)
+	for {
+		n, err := f.Read(buf)
+		if err == io.EOF {
+			fmt.Println("Error:", err)
+			break
+		}
+		fmt.Println(string(buf[:n]))
+	}
 }
