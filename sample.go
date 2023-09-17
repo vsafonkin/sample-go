77800ae5cd5d5db13a3514cff8d12bba26e5f89c
vsafonkin <vsafonkin@gmail.com>
2023-09-17 13:22:39 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("hello")
-	doSomething()
-}
 
-func doSomething() {
-	fmt.Println("do something")
+	n := 123
+
+	go func() {
+		n++
+	}()
+
+	fmt.Println(n) // race conditions
+	time.Sleep(10 * time.Millisecond)
+	fmt.Println(n)
 }
