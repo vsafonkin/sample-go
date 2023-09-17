a4da04241934355d9e18da177c92de2cd1001d92
vsafonkin <vsafonkin@gmail.com>
2023-09-17 22:31:22 +0300

    Debug

--- progressbar/progressbar.go
+++ progressbar/progressbar.go
@@ -2,6 +2,7 @@ package progressbar
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -10,12 +11,19 @@ func ProgressBar(done chan<- struct{}) {
 	mark := "*"
 	for n <= 100 {
 		fmt.Printf("\r[%d%%]%s", n, mark)
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(RandTimeout(300))
 		if n%4 == 0 {
 			mark += "*"
 		}
 		n++
 	}
-	fmt.Printf("\n")
+	fmt.Println()
 	done <- struct{}{}
+}
+
+func RandTimeout(limit int) time.Duration {
+	var timeout time.Duration
+	randInt := rand.Intn(limit)
+	timeout = time.Duration(randInt)
+	return timeout * time.Millisecond
 }
