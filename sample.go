c36b8ece32a6a268f25b4244fbcede935bcdcc6f
vsafonkin <vsafonkin@gmail.com>
2023-09-17 13:27:33 +0300

    Debug

--- sample.go
+++ sample.go
@@ -9,11 +9,14 @@ func main() {
 	fmt.Println("hello")
 
 	n := 123
+	done := make(chan struct{})
 
 	go func() {
 		n++
+		done <- struct{}{}
 	}()
 
+	<-done
 	fmt.Println(n) // race conditions
 	time.Sleep(10 * time.Millisecond)
 	fmt.Println(n)
