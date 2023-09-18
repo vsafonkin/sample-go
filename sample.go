6231db658ea7c9770c443e45d758f65c4927cd8f
vsafonkin <vsafonkin@gmail.com>
2023-09-18 14:03:19 +0300

    Debug

--- sample.go
+++ sample.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+type User struct {
+	name string
+	age  int
+}
+
 func main() {
 	fmt.Println("hello")
 	doSomething()
+
+	bob := newUser("Bob", 123)
+	fmt.Println(bob)
 }
 
 func doSomething() {
 	fmt.Println("func doSomething")
+}
+
+func newUser(name string, age int) User {
+	return User{
+		name: name,
+		age:  age,
+	}
 }
