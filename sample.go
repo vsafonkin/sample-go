0eaba1ba0791432992a50cadb57e18bc9b3a0c96
vsafonkin <vsafonkin@gmail.com>
2023-09-18 21:28:01 +0300

    Debug

--- sample.go
+++ sample.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 )
 
+type User struct {
+	name string
+}
+
 func main() {
 	fmt.Println("hello")
 	doSomething()
+
+	stc := new(struct {
+		name string
+	})
+	*stc = User{
+		name: "bob",
+	}
+	fmt.Println(stc)
 }
 
 func doSomething() {
