4f3a981759dbe5e1a021e5d1585721768707a425
vsafonkin <vsafonkin@gmail.com>
2023-09-25 21:31:12 +0300

    Debug

--- sample.go
+++ sample.go
@@ -3,14 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/pkg/profile"
-	"io"
-	"os"
 )
 
 func main() {
 	prof := profile.Start()
 	defer prof.Stop()
 
-	io.WriteString(os.Stdout, "hello\n")
-	fmt.Println("goodbye")
+	fmt.Println("hello")
 }
