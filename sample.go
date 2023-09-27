3b40e007b94776e66f387a14cc4624451f64fbe7
vsafonkin <vsafonkin@gmail.com>
2023-09-27 18:40:42 +0300

    Debug

--- sample.go
+++ sample.go
@@ -10,17 +10,4 @@ func main() {
 	defer prof.Stop()
 
 	fmt.Println("hello")
-
-	fmt.Println(sum(1, 2, 3))
-
-	nums := []int{4, 5, 6}
-	fmt.Println(sum(nums...))
-}
-
-func sum(nums ...int) int {
-	var total int
-	for _, num := range nums {
-		total += num
-	}
-	return total
 }
