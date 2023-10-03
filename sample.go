64390025d767105d12a58f22fb00e3c5db5c58fe
vsafonkin <vsafonkin@gmail.com>
2023-10-03 16:55:41 +0300

    Debug

--- sample.go
+++ sample.go
@@ -10,16 +10,4 @@ func main() {
 	defer prof.Stop()
 
 	fmt.Println("hello")
-
-	n, ptr := doSomething()
-	fmt.Printf("%T, %[1]p, %d\n", ptr, *ptr)
-	fmt.Println("n:", n)
-
-	n++
-	fmt.Println(*ptr, n)
-}
-
-func doSomething() (int, *int) {
-	n := 123
-	return n, &n
 }
