77f1c807f2bbe91438a1e4dc6b8168335b847042
vsafonkin <vsafonkin@gmail.com>
2023-10-17 16:42:38 +0300

    Debug

--- sample.go
+++ sample.go
@@ -5,6 +5,11 @@ import (
 	"github.com/pkg/profile"
 )
 
+var test = func() int {
+	fmt.Println("test func")
+	return 123
+}()
+
 func main() {
 	prof := profile.Start(profile.ProfilePath("./pprof"))
 	defer prof.Stop()
