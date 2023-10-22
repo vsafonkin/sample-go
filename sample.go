5943f5d4e04eab965eabaa952f2adee984734b0e
vsafonkin <vsafonkin@gmail.com>
2023-10-22 17:24:38 +0300

    Debug

--- sample.go
+++ sample.go
@@ -10,17 +10,15 @@ func main() {
 	defer prof.Stop()
 
 	fmt.Println("-----")
-	fmt.Println(123 + 3.45)
 
-	f := 12.3
-	res := f + 456.7
-	fmt.Printf("%v -> %[1]T\n", res)
+	doSomething("hello", "goodbye", "gogogo")
 
-	intMax := int(^uint(0) >> 1)
-	fmt.Printf("%v %08[1]b -> %[1]T\n", intMax)
-	fmt.Println(intMax + 12345)
+	names := []string{"bob", "alisa", "mike"}
+	doSomething(names...)
+}
 
-	var t byte = 253
-	sum := t + 123
-	fmt.Printf("-----\n%v -> %[1]T\n", sum)
+func doSomething(arr ...string) {
+	for i, v := range arr {
+		fmt.Printf("%d: %s\n", i, v)
+	}
 }
