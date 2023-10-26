59dde39ae5a42fc3526b2b6c7a8f8bc170ddfd32
vsafonkin <vsafonkin@gmail.com>
2023-10-26 15:39:04 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"unsafe"
 )
 
 func main() {
@@ -12,19 +10,5 @@ func main() {
 	var b = 253
 	var c = 123
 	var sum = a + b + c
-	fmt.Printf("Size: %d, Pointer: %p\n", unsafe.Sizeof(sum), &sum)
-	doSomething()
-
-	str := "goodbye"
-	fmt.Println(str)
-}
-
-func doSomething() {
-	message := "hibob"
-	userId := 123321
-	fmt.Println("start...", message, &message, userId, &userId)
-	for {
-		fmt.Println(message, &message, userId, &userId)
-		time.Sleep(5 * time.Second)
-	}
+	fmt.Println(sum)
 }
