8da9021176b7d059d8b32b010f1f15e5664819cd
vsafonkin <vsafonkin@gmail.com>
2023-11-02 00:00:15 +0300

    Debug

--- sample.go
+++ sample.go
@@ -3,30 +3,8 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"sync"
-	"time"
 )
 
-type Counter struct {
-	sync.Mutex
-	c int
-}
-
 func main() {
-	counter := Counter{
-		c: 0,
-	}
-
-	go func() {
-		defer counter.Unlock()
-		counter.Lock()
-		for i := 0; i < 100000; i++ {
-			counter.c = i
-		}
-	}()
-
-	time.Sleep(10 * time.Nanosecond)
-	counter.Lock()
-	fmt.Printf("Counter: %d, goroutines: %d\n", counter.c, runtime.NumGoroutine())
-	counter.Unlock()
+	fmt.Printf("goroutines: %d\n", runtime.NumGoroutine())
 }
