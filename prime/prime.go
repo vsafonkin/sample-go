1581311cc6b6c427733ddff035fd77184c7700e4
vsafonkin <vsafonkin@gmail.com>
2023-11-10 13:43:08 +0300

    Prime

---
+++ prime/prime.go
@@ -0,0 +1,52 @@
+package prime
+
+import "sync"
+
+func IsPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	isPrime := true
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return isPrime
+}
+
+func PrimeSlice(n int) []int {
+	var output []int
+	for i := 2; i <= n; i++ {
+		if IsPrime(i) {
+			output = append(output, i)
+		}
+	}
+	return output
+}
+
+func ConcPrimeSlice(n int) []int {
+	var output []int
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	for i := 2; i <= n; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			if IsPrime(i) {
+				ch <- i
+			}
+		}(i)
+	}
+
+	go func() {
+		for v := range ch {
+			output = append(output, v)
+		}
+	}()
+
+	wg.Wait()
+	close(ch)
+	return output
+}
--- sample.go
+++ sample.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/vsafonkin/sample-go/prime"
 )
 
 func main() {
 	fmt.Println("---")
+	n := 100_000_000
+	slc := prime.PrimeSlice(n)
+	fmt.Println(len(slc))
 }
