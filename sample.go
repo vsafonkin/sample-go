a400a86e2b243f2e365e3f6930cd7435d0082afe
vsafonkin <vsafonkin@gmail.com>
2023-11-15 18:10:41 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("---")
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "https://2ip.ru", nil)
+	if err != nil {
+		log.Fatal("create request error:", err)
+	}
+
+	req.Header.Add("user-agent", "curl")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("request error:", err)
+	}
+
+	buf := make([]byte, 1500)
+	n, err := resp.Body.Read(buf)
+	if err != nil {
+		log.Fatal("read body error:", err)
+	}
+	fmt.Println(string(buf[:n:n]))
 }
