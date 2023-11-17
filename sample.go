6eb189c473a2e80c3e450554b7485956544a0ba6
vsafonkin <vsafonkin@gmail.com>
2023-11-17 11:13:31 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,33 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"time"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println("---")
 
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, "https://2ip.ru", nil)
-	if err != nil {
-		log.Fatal("create request error:", err)
-	}
-
-	req.Header.Add("user-agent", "curl")
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("request error:", err)
-	}
-
-	buf := make([]byte, 1500)
-	n, err := resp.Body.Read(buf)
-	if err != nil {
-		log.Fatal("read body error:", err)
-	}
-	fmt.Println(string(buf[:n:n]))
+	str := "hello"
+	p := (*[2]int)(unsafe.Pointer(&str))
+	p[1] = 48000
+	fmt.Println(str)
 }
