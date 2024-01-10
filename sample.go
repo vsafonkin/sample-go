d9f3e4c551010d9f05ff7c398aabd1de9235351c
vsafonkin <vsafonkin@gmail.com>
2024-01-10 21:32:08 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("-----")
 
-	var count int
-	for {
-		fmt.Println(count)
-		count++
-		time.Sleep(1 * time.Second)
-	}
+	b := 0b01111011
+	fmt.Println(b + 1.2e3)
 }
