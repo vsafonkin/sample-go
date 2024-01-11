f0f4abd873905757a3d0e4ba9caccece5999c73c
vsafonkin <vsafonkin@gmail.com>
2024-01-11 10:44:17 +0300

    Debug

--- sample.go
+++ sample.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	fmt.Println("-----")
 
-	var mb byte = 199
+	var bt byte = 199
 	var rn rune = 'W'
 	a := 123
 	b := 231
 	str := "hello, golang"
 
-	fmt.Println(mb, rn, a, b, str)
+	fmt.Println(bt, rn, a, b, str)
 }
