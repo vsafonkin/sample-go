9905f40d3b4d3ca98a996ff8951b3d2aa33293b3
vsafonkin <vsafonkin@gmail.com>
2024-01-12 15:42:41 +0300

    Debug

--- sample.go
+++ sample.go
@@ -7,14 +7,17 @@ import (
 func main() {
 	fmt.Println("-----")
 
-	var arr [3]int
+	var arr []int = []int{0, 0, 0}
 	fmt.Println(arr)
 
 	arr[1] = 123
 	fmt.Println(arr)
 
-	func(arr [3]int) {
+	func(arr []int) {
 		arr[2] = 456
+		fmt.Println(cap(arr))
+		arr = append(arr, 789)
+		fmt.Println(cap(arr))
 	}(arr)
 
 	fmt.Println(arr)
