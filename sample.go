986de2ed643bd1681d7afedebf0431d23cee21b4
vsafonkin <vsafonkin@gmail.com>
2024-01-23 15:06:47 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("-----")
-	s1 := doSomething()
-	println(s1)
+	s := []int{1, 3, 5, 6, 9, 12, 13, 17, 24, 87, 99}
+	if len(os.Args) > 1 {
+		target, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Println(searchInsert(s, target))
+	}
 }
 
-//go:noinline
-func doSomething() [][]int {
-
-	t := make([]int, 1000)
-	n := 1000
-	s := make([]int, n)
-	for i := 0; i < n; i++ {
-		s[i] = i
-		t[i] = i
+func searchInsert(nums []int, target int) int {
+	i := len(nums) / 2
+	switch {
+	case i == 0:
+		if nums[0] < target {
+			return 1
+		}
+		return 0
+	case nums[i] > target:
+		out := searchInsert(nums[:i], target)
+		return out
+	case nums[i] < target:
+		return searchInsert(nums[i:], target) + i
 	}
-	return [][]int{t, s, run()}
-}
-
-//go:noinline
-func run() []int {
-	n := 5000
-	s := make([]int, n)
-	for i := 0; i < n; i++ {
-		s[i] = i
-	}
-	return s
+	return i
 }
