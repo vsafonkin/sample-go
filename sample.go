6b97963703e8857ab4d987e31de8c7d5016dc668
vsafonkin <vsafonkin@gmail.com>
2024-01-29 08:59:12 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
 )
 
-type CustomErr struct {
-	message string
-	code    int
-}
-
 func main() {
-	err := errors.New("test error")
-	fmt.Println(err)
-	println(err)
-
-	var e error
-	println(e)
-
-	var ce *CustomErr
-	println(ce)
-
-	e = ce
-	println(e)
-}
-
-func (ce *CustomErr) Error() string {
-	return ce.message
+	ctx := context.Background()
+	fmt.Printf("%+v\n", ctx)
 }
