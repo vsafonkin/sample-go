f5f0636fca970a5b4bf76eb79f166eca7dab0f74
vsafonkin <vsafonkin@gmail.com>
2024-01-30 11:12:07 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 func main() {
-	ctx := context.Background()
-	fmt.Printf("%+v\n", ctx)
+	var wg sync.WaitGroup
+
+	var once sync.Once
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			once.Do(func() {
+				fmt.Println("once function")
+			})
+		}()
+	}
+	fmt.Println(runtime.NumGoroutine())
+	wg.Wait()
+	fmt.Println("done")
 }
