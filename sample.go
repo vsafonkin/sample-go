9cf620882b55a6631de2ba798da7a697077ef7e9
vsafonkin <vsafonkin@gmail.com>
2024-02-06 11:30:21 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("---")
+	fmt.Println(Persistence(39))
+}
+
+func Persistence(n int) int {
+	var counter, acc = 1, 1
+	s := fmt.Sprintf("%d", n)
+	if len(s) == 1 {
+		return 0
+	}
+	for _, v := range s {
+		d, _ := strconv.Atoi(string(v))
+		acc *= d
+	}
+	counter += Persistence(acc)
+	return counter
 }
