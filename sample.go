cb2e6d909f311144eb4134fe97eb8eac6fcf72a9
vsafonkin <vsafonkin@gmail.com>
2024-02-24 19:03:26 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,24 +1,3 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(wave("hello"))
-	fmt.Println(wave("goodbye"))
-	fmt.Println(wave(" x yz"))
-}
-
-func wave(words string) []string {
-	ln := len(words)
-	var result []string
-
-	for i := 0; i < ln; i++ {
-		if words[i] == ' ' {
-			continue
-		}
-		result = append(result, words[:i]+string(words[i]-32)+words[i+1:])
-	}
-	return result
-}
+func main() {}
