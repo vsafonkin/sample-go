b5250e8e6b197d3ba2818d2c3c1ac3d0d6843adc
vsafonkin <vsafonkin@gmail.com>
2024-03-14 16:50:07 +0300

    Debug

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/vsafonkin/sample-go
 
-go 1.20
+go 1.22
 
 require (
 	github.com/golang/mock v1.6.0
--- sample.go
+++ sample.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	n := 10
+	done := make(chan int)
+	var counter int
+	for i := 0; i < n; i++ {
+		go func() {
+			done <- i
+		}()
+	}
+
+	for v := range done {
+		fmt.Println(v)
+		counter++
+		if counter == n {
+			close(done)
+		}
+	}
+}
