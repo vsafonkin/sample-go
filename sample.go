45f539ba0873a6b7b5c2188da4ae96532492491c
vsafonkin <vsafonkin@gmail.com>
2024-03-14 17:32:55 +0300

    Debug

--- sample.go
+++ sample.go
@@ -3,20 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	n := 10
-	done := make(chan int)
-	var counter int
 	for i := range 10 {
-		go func() {
-			done <- i
-		}()
-	}
-
-	for v := range done {
-		fmt.Println(v)
-		counter++
-		if counter == n {
-			close(done)
-		}
+		fmt.Println(i)
 	}
 }
