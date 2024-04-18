7410a5e814a366111fd5418b0d4549947470572d
vsafonkin <vsafonkin@gmail.com>
2024-04-19 01:45:16 +0300

    Debug

--- sample.go
+++ sample.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var s []int
+	var temp int
+	for i := range 8192 {
+		s = append(s, i)
+		if temp != cap(s) {
+			fmt.Println(cap(s))
+			temp = cap(s)
+		}
+	}
+}
