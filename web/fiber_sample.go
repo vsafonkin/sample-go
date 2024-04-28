4d66d109bb0a8a938c515ba1316243e53a23d72b
vsafonkin <vsafonkin@gmail.com>
2024-04-28 17:17:27 +0300

    web

--- web/fiber_sample.go
+++ web/fiber_sample.go
@@ -4,22 +4,38 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	// "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 type responce struct {
 	Message string
+	Data    any
+}
+
+type User struct {
+	Id   int
+	Name string
 }
 
 func RunFiberServer(host string, port string) {
 	r := fiber.New()
-	// r.Use(logger.New())
+	r.Use(logger.New())
 
-	r.Get("/", home)
+	r.Get("/", homeGet)
+	r.Post("/", homePost)
 
 	r.Listen(fmt.Sprintf("%s:%s", host, port))
 }
 
-func home(c *fiber.Ctx) error {
-	return c.JSON(responce{Message: "Hello, Bob"})
+func homeGet(c *fiber.Ctx) error {
+	return c.JSON(responce{Message: "Hello"})
+}
+
+func homePost(c *fiber.Ctx) error {
+
+	var user User
+	if err := c.BodyParser(&user); err != nil {
+		fmt.Println("parser error:", err)
+	}
+	return c.JSON(responce{Message: user.Name, Data: user.Id})
 }
