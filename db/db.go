2ab8a001f115c0afa026b2370b351629b6ec25cd
vsafonkin <vsafonkin@gmail.com>
2024-05-01 13:05:12 +0300

    Debug

--- db/db.go
+++ db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"context"
 	"fmt"
 	"time"
 
@@ -47,80 +46,4 @@ func (db *DB) Row(tablename string, column string, value string) map[string]any 
 	out := make(map[string]any, 1)
 	db.conn.Table(tablename).Take(out, column, value)
 	return out
-}
-
-func NewPool(n int, config Config) ([]*DB, error) {
-	var connPool []*DB
-	appname := config.AppName
-	for i := range n {
-		config.AppName = fmt.Sprintf("%s_%d", appname, i)
-		conn, err := NewConnect(config)
-		if err != nil {
-			fmt.Println("appname:", config.AppName, "connection error:", err)
-			continue
-		}
-		connPool = append(connPool, conn)
-	}
-	if len(connPool) == 0 {
-		return nil, fmt.Errorf("there are no any successful connections, pool is empty")
-	}
-	return connPool, nil
-}
-
-func TestLoad(ctx context.Context, query string, pool []*DB, freq time.Duration) {
-	num := len(pool)
-	var currentTime, minTime, maxTime time.Duration
-	var counter int
-	go func() {
-		minTime = 1000 * time.Second
-	out:
-		for {
-			if currentTime > maxTime {
-				maxTime = currentTime
-			}
-			if currentTime < minTime && currentTime != 0 {
-				minTime = currentTime
-			}
-			fmt.Printf("counter: %v, time: %v, min time: %v, max time: %v    \r",
-				counter,
-				currentTime,
-				minTime,
-				maxTime)
-			select {
-			case <-ctx.Done():
-				fmt.Println()
-				break out
-			default:
-				time.Sleep(100 * time.Millisecond)
-			}
-		}
-	}()
-
-	for i := range num {
-		go func() {
-			conn := pool[i]
-			defer func() {
-				sql, err := conn.conn.DB()
-				if err != nil {
-					fmt.Println("close connection error:", err)
-					return
-				}
-				sql.Close()
-			}()
-		out:
-			for {
-				_, currentTime = conn.ExecRawQuery(query)
-				select {
-				case <-ctx.Done():
-					break out
-				default:
-					time.Sleep(freq)
-					counter++
-				}
-			}
-		}()
-	}
-	select {
-	case <-ctx.Done():
-	}
 }
