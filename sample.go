c082803f34765d05177f4a60bfebde3c26ac1c9c
vsafonkin <vsafonkin@gmail.com>
2024-05-10 14:58:43 +0300

    Errors

--- sample.go
+++ sample.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type CustomErr struct {
+	Message string
+}
 
 func main() {
 	fmt.Println("---")
+
+	var err error
+	err = CustomErr{
+		Message: "hello, alice",
+	}
+	err = fmt.Errorf("wrap error: %w", err)
+	err = fmt.Errorf("second wrap error: %w", err)
+	fmt.Println(err)
+
+	fmt.Println(errors.Is(err, CustomErr{}))
+	fmt.Println(errors.As(err, &CustomErr{}))
+}
+
+func (e CustomErr) Error() string {
+	return "custom error"
 }
