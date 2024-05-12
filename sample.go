3585355b16f2ca68f60329f961adb93c37880bd9
vsafonkin <vsafonkin@gmail.com>
2024-05-12 14:20:45 +0300

    Binary tree

--- sample.go
+++ sample.go
@@ -4,6 +4,54 @@ import (
 	"fmt"
 )
 
+type Tree struct {
+	v int
+	l *Tree
+	r *Tree
+}
+
 func main() {
-	fmt.Println()
+	tree := Tree{
+		v: 10,
+	}
+	fmt.Println(tree)
+	tree.Insert(13)
+	tree.Insert(8)
+	tree.Insert(6)
+	fmt.Println(tree)
+	fmt.Println(tree.Sum())
+}
+
+func (t *Tree) Insert(n int) {
+	if t == nil {
+		return
+	}
+	if n < t.v {
+		if t.l == nil {
+			t.l = &Tree{
+				v: n,
+			}
+			return
+		}
+		t.l.Insert(n)
+		return
+	}
+	if n > t.v {
+		if t.r == nil {
+			t.r = &Tree{
+				v: n,
+			}
+			return
+		}
+		t.r.Insert(n)
+		return
+	}
+	t.v = n
+}
+
+func (t *Tree) Sum() int {
+	if t == nil {
+		return 0
+	}
+	return t.v + t.l.Sum() + t.r.Sum()
 }
