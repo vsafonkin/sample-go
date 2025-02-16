890387c8b59893d0fad55f0116ea11d9b43b3d78
vsafonkin <vsafonkin@gmail.com>
2025-02-16 19:29:53 +0300

    Debug

--- sample.go
+++ sample.go
@@ -2,13 +2,55 @@ package main
 
 import (
 	"fmt"
-	"github.com/vsafonkin/sample-go/web"
+	"github.com/goccy/go-json"
 	"log"
+	"os"
 )
 
+type Users []User
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
-	fmt.Println("Hello, go")
-	if err := web.RunGinServer("0.0.0.0", "8083"); err != nil {
-		log.Fatal()
+	path := "./test_data/users.json"
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		fmt.Println("defer")
+	}()
+
+	users := Users{}
+
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", users)
+
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var users2 Users
+	err = json.NewDecoder(f).Decode(&users2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, user := range users2 {
+		fmt.Printf("id: %d, name: %s\n", user.Id, user.Name)
 	}
 }
